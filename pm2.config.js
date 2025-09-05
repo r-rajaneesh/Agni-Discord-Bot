f96804466a61754a0f3b87a17e7968a156d73b53
r-rajaneesh <rajaneeshr2006@gmail.com>
2025-09-05 22:56:18 +0530

    🔧 Tweaked pm2 config.

--- pm2.config.js
+++ pm2.config.js
@@ -1,7 +1,8 @@
 export default {
 	name: "Agni Discord Bot",
-	script: "index.ts",
-	interpreter: "bun",
+	script: "bun",
+	args: "index.js",
+	interpreter: "none",
 	env: {
 		PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
 	},
